75c4c88091913821adff486862298245b1085deb
Marton Gombos <marton.gombos@yahoo.com>
2023-06-27 19:34:20 +0200

    models in separate file

--- app/page.tsx
+++ app/page.tsx
@@ -8,33 +8,6 @@ import CategorySelect from "@/components/CategorySelect";
 import { RxCross2 } from "react-icons/rx";
 import { MdOutlineDoneOutline } from "react-icons/md";
 
-type ApiResponse = {
-  response_code: number;
-  results: Question[];
-};
-
-type Question = {
-  category: string;
-  type: string;
-  difficulty: string;
-  question: string;
-  correct_answer: string;
-  incorrect_answers: string[];
-};
-
-enum categoryList {
-  "General Knowledge" = 9,
-  "Books" = 10,
-  "Film" = 11,
-  "Music" = 12,
-  "Television" = 14,
-  "Video Games" = 15,
-  "Board Games" = 16,
-  "Computers" = 18,
-  "Mythology" = 20,
-  "Sports" = 21,
-}
-
 export default function TrueFalseApp() {
   const [questions, setQuestions] = useState<Question[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
